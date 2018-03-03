ee42bc27defb660e5bac2fa1989c3c6c84f029dc
donchan922 <31620041+donchan922@users.noreply.github.com>
2018-03-03 22:26:25 +0900

    Bug fix

--- tigers_report.py
+++ tigers_report.py
@@ -3,11 +3,10 @@ from bs4 import BeautifulSoup
 import re
 
 def main():
-	message = createReportMessage()
-	print(message)
+  message = createReportMessage()
+  print(message)
     
 def createReportMessage():
-    teamNameList = []
     teamPointList = []
 
     url = 'https://m.hanshintigers.jp/game/score/'
@@ -17,44 +16,21 @@ def createReportMessage():
     soup = BeautifulSoup(request.text, 'lxml')
     # now inning
     inning = soup.find(class_='inning').text
-    # get team names
-    for i in soup.find_all('img', src=re.compile("/images/logo/logo_")):
-        teamNameList.append(i['src'])
-
-    # get team points
+    # each team point list
+    teamPointList = []
+    # get each team points
     for i in soup.find_all(class_='number'):
         teamPointList.append(i.text)
-
-    # set team names and points
-    for i, team in enumerate(teamNameList):
-        # TIGERS
-        if team == '/images/logo/logo_t.png':
-            tigers_name = "阪神"
-            tigers_number = teamPointList[i]
-        # CARP
-        elif team == '/images/logo/logo_c.png':
-            opponent_name = "広島"
-            opponent_number = teamPointList[i]
-        # GIANTS
-        elif team == '/images/logo/logo_g.png':
-            opponent_name = "巨人"
-            opponent_number = teamPointList[i]
-        # DRAGONS
-        elif team == '/images/logo/logo_d.png':
-            opponent_name = "中日"
-            opponent_number = teamPointList[i]
-        # SWAROWS
-        elif team == '/images/logo/logo_s.png':
-            opponent_name = "ヤクルト"
-            opponent_number = teamPointList[i]
-        # BAYSTARS
-        elif team == '/images/logo/logo_db.png':
-            opponent_name = "横浜"
-            opponent_number = teamPointList[i]
+    
+    home_team_name = soup.find(class_='l_left').text
+    away_team_name = soup.find(class_='l_right').text
+    home_team_point = teamPointList[1]
+    away_team_point = teamPointList[0]
 
     # generate message
-    message = inning + '\n' + tigers_name + tigers_number + ' - ' + opponent_number + opponent_name
+    message = inning + '\n' + home_team_name + home_team_point + ' - ' + away_team_name + away_team_point
+
     return message
 
 if __name__ == '__main__':
-	main()
+  main()
